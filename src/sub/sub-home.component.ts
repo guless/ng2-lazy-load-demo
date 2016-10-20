39042f5b7a6454760920babae780f65b6e7ce8b9
winpzs <winpzs@gmail.com>
2016-10-20 15:42:27 +0800

    test

--- src/sub/sub-home.component.ts
+++ src/sub/sub-home.component.ts
@@ -3,8 +3,9 @@ import { Component } from "@angular/core";
 @Component({
   selector: "sub-home",
   template: `
-    <h3>Sub Home Componentsdfsdf Test!!!!!</h3>
+    <h3>Sub Home Components {{hello}}</h3>
   `
 })
 export class SubHomeComponent {
+  hello:string = "hello ok!";
 }
